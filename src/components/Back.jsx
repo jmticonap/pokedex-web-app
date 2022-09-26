c8caa4484dff331215ee04abeca4e72e81f788b6
Kevin del Cid <danieldelcidf@gmail.com>
2022-09-25 23:00:55 -0600

    avance profile ++

---
+++ src/components/Back.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ArrowBack } from "@mui/icons-material";
+import { Fab } from "@mui/material";
+
+const Back = () => {
+  const navigate = useNavigate();
+  return (
+    <Fab
+      color="#dd1a1a"
+      className="sss"
+      onClick={() => navigate(-1)}
+      aria-label="add"
+    >
+      <ArrowBack />
+    </Fab>
+  );
+};
+
+export default Back;
--- src/components/PokemonProfile.jsx
+++ src/components/PokemonProfile.jsx
@@ -3,8 +3,45 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./css/pokemonProfile.css";
 import { cardBackgoundStyle, capitalize } from "../utils";
-
+import { ArrowBack } from "@mui/icons-material";
+import { Fab } from "@mui/material";
 import Header from "./Header";
+import Back from "./Back";
+
+const ProgressBars = ({ stat }) => {
+  const [beforeAnimation, setBeforeAnimation] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      //setBeforeAnimation((stat?.base_state * 100) / 250);
+      const realValue = (stat.base_stat * 100) / 250;
+
+      setBeforeAnimation(realValue);
+    }, 500);
+  }, []);
+
+  const calculateWidthByStat = () => {
+    const total = (beforeAnimation * 100) / 250;
+    return total + "%";
+  };
+
+  return (
+    <div className="stat-container">
+      <div className="properties-stats-container">
+        <h3>{stat.stat.name}</h3>
+        <h3>{stat.base_stat}/250</h3>
+      </div>
+      <div className="posible-bar-stat">
+        <div
+          className="bar-stat bar-animation "
+          style={{
+            width: `${beforeAnimation}%`,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 const PokemonProfile = () => {
   const name = useParams("name").name;
@@ -32,10 +69,6 @@ const PokemonProfile = () => {
     }
   };
   console.log(pokemon);
-  const calculateWidthByStat = (base_state) => {
-    const total = (base_state * 100) / 250;
-    return total + "%";
-  };
 
   return (
     <div>
@@ -50,80 +83,80 @@ const PokemonProfile = () => {
       ) : (
         <div>
           <Header />
+          <Back />
+          <main className="main-profile-container">
+            <div className="card-profile-primary">
+              <div className={`background-poke ${getStyleByKey("background")}`}>
+                <img
+                  className="imgProfile"
+                  src={
+                    pokemon.sprites?.other["official-artwork"]["front_default"]
+                  }
+                  alt={name}
+                />
+              </div>
 
-          <div className="card-profile-primary">
-            <div className={`background-poke ${getStyleByKey("background")}`}>
-              <img
-                className="imgProfile"
-                src={
-                  pokemon.sprites?.other["official-artwork"]["front_default"]
-                }
-                alt={name}
-              />
+              <section className="info-profile">
+                <div className="data1-profile">
+                  <h2 className="title2-profile">#{pokemon.id}</h2>
+                  <hr className="hr-absolute"/>
+                  <h1 className="title-profile">{pokemon.name}</h1>
+
+                  <div className="data-profile">
+                    <div>
+                      <h4 className="sub-title-profile">Weight</h4>
+                      <p>{pokemon.weight}</p>
+                    </div>
+                    <div>
+                      <h4 className="sub-title-profile">Height</h4>
+                      <p>{pokemon.height}</p>
+                    </div>
+                  </div>
+                  <div className="data2-profile">
+                    <div className="types-profile-container">
+                      <h3 className="title3-profile">Type</h3>
+                      <div className="types-profile">
+                        {pokemon.types.map((type) => (
+                          //pendiente poner color segun la clase
+                          <div key={type.type.name}>{type.type.name}</div>
+                        ))}
+                      </div>
+                    </div>
+
+                    <div className="hability-profile-container">
+                      <h3 className="title3-profile">Skills</h3>
+                      <div className="types-profile">
+                        {pokemon.abilities.map((skill) => (
+                          //pendiente poner color segun la clase
+                          <div key={skill.ability.name}>
+                            {skill.ability.name}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+              <section className="stats-section">
+                <h2>
+                  Stats <hr />
+                </h2>
+                <div className="stats-container">
+                  {pokemon.stats.map((stat) => (
+                    <ProgressBars key={stat.stat.name} stat={stat} />
+                  ))}
+                </div>
+              </section>
             </div>
-            <section className="info-profile">
-              <div className="data1-profile">
-                <h2 className="title2-profile">#{pokemon.id}</h2>
-                <h1 className="title-profile">{pokemon.name}</h1>
-
-                <div className="data-profile">
-                  <div>
-                    <h4 className="sub-title-profile">Weight</h4>
-                    <p>{pokemon.weight}</p>
-                  </div>
-                  <div>
-                    <h4 className="sub-title-profile">Height</h4>
-                    <p>{pokemon.height}</p>
-                  </div>
+            <section className="moves-section-container">
+              {pokemon.moves.map((move) => (
+                <div key={move[0]?.move.url} className="move-card">
+                  <p>{move.move.name}</p>
                 </div>
-                <div className="data2-profile">
-                  <div className="types-profile-container">
-                    <h3 className="title3-profile">Type</h3>
-                    <div className="types-profile">
-                      {pokemon.types.map((type) => (
-                        //pendiente poner color segun la clase
-                        <div key={type.type.name}>{type.type.name}</div>
-                      ))}
-                    </div>
-                  </div>
-
-                  <div className="hability-profile-container">
-                    <h3 className="title3-profile">Skills</h3>
-                    <div className="types-profile">
-                      {pokemon.abilities.map((skill) => (
-                        //pendiente poner color segun la clase
-                        <div key={skill.ability.name}>{skill.ability.name}</div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </section>
-
-            <section className="stats-section">
-              <h2>
-                Stats <hr />
-              </h2>
-              <div className="stats-container">
-                {pokemon.stats.map((stat) => (
-                  <div key={stat.stat.name} className="stat-container">
-                    <div className="properties-stats-container">
-                      <h3>{stat.stat.name}</h3>
-                      <h3>{stat.base_stat}/250</h3>
-                    </div>
-                    <div className="posible-bar-stat">
-                      <div
-                        className="bar-stat bar-animation "
-                        style={{
-                          width: calculateWidthByStat(stat.base_stat),
-                        }}
-                      ></div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </section>
-          </div>
+          </main>
         </div>
       )}
     </div>
